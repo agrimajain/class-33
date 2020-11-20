aa33daa5d5b8da62a5bfca656e5135fbbed776fd
agrimajain <agrima.jain04@gmail.com>
2020-11-20 18:33:03 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//variables
 var bricks;
 var bricks_group;
 var coins, coinImg;
@@ -6,14 +7,20 @@ var coins_group;
 var score=0;
 //var mushroom, turtles
 var obstaclesGroup
+var gameState="play"
+//to load all sounds,animations and pictures
 function preload(){
-    mario_running = loadAnimation("images/mar1.png", "images/mar2.png", "images/mar3.png", "images/mar4.png", "images/mar5.png", "images/mar6.png", "images/mar7.png");
+    // animation and images
+     mario_running = loadAnimation("images/mar1.png", "images/mar2.png", "images/mar3.png", "images/mar4.png", "images/mar5.png", "images/mar6.png", "images/mar7.png");
      bgImage = loadImage("images/bgnew.png");
      bricks_img = loadImage("images/brick.png")
      coinImg =loadAnimation("images/con1.png","images/con2.png","images/con3.png","images/con4.png","images/con5.png","images/con6.png");
-     coinSounds = loadSound("sounds/coinSound.mp3")
      mushObstacleImage = loadAnimation("images/mush1.png","images/mush2.png","images/mush3.png","images/mush4.png","images/mush5.png","images/mush6.png",);
      turObstacleImage = loadAnimation("images/tur1.png","images/tur2.png","images/tur3.png","images/tur4.png","images/tur5.png");
+     //sounds
+     dieSound =loadSound("sounds/dieSound.mp3")
+     coinSounds = loadSound("sounds/coinSound.mp3")
+     //groups
      bricks_group=new Group();
      coins_group=new Group();
      obstaclesGroup=new Group();
@@ -21,7 +28,8 @@ function preload(){
 
 
 function setup() {
-createCanvas(1000, 600);
+//create canvas or background
+   createCanvas(1000, 600);
 bg= createSprite(500, 300);
 bg.addImage(bgImage);
 mario = createSprite(200, 505, 20,50);
@@ -33,46 +41,63 @@ ground.visible = false;
 }
 
 function draw() {
-   if( keyDown("space")){
-      mario.velocityY = -16; }
-   mario.velocityY = mario.velocityY +0.5;
-   mario.collide(ground);
-   //to make bg move(speed and time given)
-   bg.velocityX= -6;
-   if(bg.x < 100){
-      bg.x=bg.width/4;
-   }
-   //to make or generate the bricks
-   generatebricks();
-   //for loop is to collide mario with bricks
-   for(var i=0; i<bricks_group.length;i++ ){
-      var temp = (bricks_group).get(i) ;
-      if (temp.isTouching(mario))
-      { 
-         mario.collide(temp); 
+   if(gameState==="play"){
+      if( keyDown("space")){
+         mario.velocityY = -16; }
+      mario.velocityY = mario.velocityY +0.5;
+      mario.collide(ground);
+      //to make bg move(speed and time given)
+      bg.velocityX= -6;
+      if(bg.x < 100){
+         bg.x=bg.width/4;
       }
-   }
-   generateCoins();
-   //to destroy the coins when mario touch
-   for(var t=0; t<coins_group.length;t++){
-      var rest = (coins_group).get(t);
-      if (rest.isTouching(mario));
-      {  score++;
-         coinSounds.play();
-         rest.destroy();
+      //to make or generate the bricks
+      generatebricks();
+      //for loop is to collide mario with bricks
+      for(var i=0; i<bricks_group.length;i++ ){
+         var temp = (bricks_group).get(i) ;
+         if (temp.isTouching(mario))
+         { 
+            mario.collide(temp); 
+         }
       }
-   }
-   
-   generateObstacles()
+      generateCoins();
+      //to destroy the coins when mario touch
+      for(var t=0; t<coins_group.length;t++){
+         var rest = (coins_group).get(t);
+         if (rest.isTouching(mario));
+         {  score++;
+            coinSounds.play();
+            rest.destroy();
+         }
+      }
       
-   
-   if (mario.x < 200)
-   { 
-      mario.x = 200; 
+      generateObstacles()
+         
+      
+      if (mario.x < 200)
+      { 
+         mario.x = 200; 
+      }
+      if (mario.y < 50);
+      {
+         mario.y = 50; 
+      }
+
+      if(obstaclesGroup.isTouching(mario)){
+         dieSound.play();
+         gameState="end";
+      }
+      
    }
-   if (mario.y < 50)
-   {
-       mario.y = 50; 
+   //when game in end state
+   else if(gameState=="end"){
+      mario.velocityX=0;
+      mario.velocityY=0;
+      bg.velocityX=0;
+      obstaclesGroup.setVelocityXEach(0);
+      coins_Group.setVelocityXEach(0);
+      bricks_Group.setVelocityXEach(0);
    }
    drawSprites()
 
@@ -93,7 +118,7 @@ function generateCoins(){
   //to generate coins
    if(frameCount % 40==0){
       coins=createSprite(1200,random(80,350),40,40);
-      coins.addAnimation(coinImg)
+      coins.addAnimation(coinImg);
       coins.velocityX= -6;
       coins.lifetime=250;
       coins_group.add(coins)
